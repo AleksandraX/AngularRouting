01a87fe694f7d83325040f11ae132fa107297b6f
Aleksandra <aleksandra.czerwa@gmail.com>
2022-03-17 22:03:18 +0100

    Akita added
    Akita added, Angular version downgraded from 13 to 12, schematics of Akita works

---
+++ good-start/src/app/session/state/session.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SessionStore } from './session.store';
+
+@Injectable({ providedIn: 'root' })
+export class SessionService {
+
+  constructor(private sessionStore: SessionStore,
+              private http: HttpClient) {
+  }
+
+}
